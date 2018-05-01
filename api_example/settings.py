1cf8de8fc09699faa22243f2323ee828cdc47f66
Chittipobng014 <tpl_army@hotmail.com>
2018-05-02 04:22:01 +0700

    remove sh*t in setting

--- api_example/settings.py
+++ api_example/settings.py
@@ -40,15 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'ProductReview',
-    'pipeline',
-    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.BrokenLinkEmailsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
