b2e0101201f2c78944b136be25b9746ed417b4c8
Chittipobng014 <tpl_army@hotmail.com>
2018-05-05 15:44:59 +0700

    add rest

--- api_example/settings.py
+++ api_example/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4(4zavz#1(zv#1w^##v*(#j&uu=fyi29v-^xaxmn1=y_92zf_z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'pacific-peak-27279.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'pacific-peak-27279.herokuapp.com', 'productreviewapi.herokuapp.com']
 
 
 # Application definition
